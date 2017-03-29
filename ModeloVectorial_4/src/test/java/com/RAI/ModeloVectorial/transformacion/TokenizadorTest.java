c328bb902c01071ae78e6e68b6963e54f4f2d238
vdegou <vdegou@gmail.com>
2017-03-29 18:13:46 +0200

    Added JUnit test for Tokenizador class.

---
+++ ModeloVectorial_4/src/test/java/com/RAI/ModeloVectorial/transformacion/TokenizadorTest.java
@@ -0,0 +1,66 @@
+package com.RAI.ModeloVectorial.transformacion;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TokenizadorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRemoveStopWords() throws Exception {
+		String originalString = "The quick brown fox jumps over a dog.";
+		
+		String expected = "quick brown fox jumps over dog ";
+		String result = Tokenizador.removeStopWords(originalString);
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testStemTerm() {
+		String example1 = "catty";
+		String example2 = "conflated";
+		String example3 = "falling";
+		String example4 = "sized";
+		String example5 = "happy";
+		
+		String expected1 = "catti";
+		String expected2 = "conflat";
+		String expected3 = "fall";
+		String expected4 = "size";
+		String expected5 = "happi";
+		
+		String result1 = Tokenizador.stemTerm(example1);
+		String result2 = Tokenizador.stemTerm(example2);
+		String result3 = Tokenizador.stemTerm(example3);
+		String result4 = Tokenizador.stemTerm(example4);
+		String result5 = Tokenizador.stemTerm(example5);
+		
+		assertEquals(expected1, result1);
+		assertEquals(expected2, result2);
+		assertEquals(expected3, result3);
+		assertEquals(expected4, result4);
+		assertEquals(expected5, result5);
+	}
+
+}
