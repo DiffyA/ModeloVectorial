ea5f8051d4865c1ea4fee84b0c85c20093b9e121
vdegou <vdegou@gmail.com>
2017-04-02 01:47:56 +0200

    Added test coverage.

--- ModeloVectorial_4/src/test/java/com/RAI/ModeloVectorial/diccionario/DiccionarioTest.java
+++ ModeloVectorial_4/src/test/java/com/RAI/ModeloVectorial/diccionario/DiccionarioTest.java
@@ -1,5 +1,11 @@
 package com.RAI.ModeloVectorial.diccionario;
 
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.lucene.index.Term;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -10,8 +16,7 @@ import com.RAI.ModeloVectorial.core.Documento;
 
 public class DiccionarioTest {
 	private Diccionario dicc = new Diccionario();
-	private Documento doc1 = new Documento("src/test/resources/test.html");
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -30,12 +35,59 @@ public class DiccionarioTest {
 
 	@Test
 	public void testAddDictionaryEntry() {
-		String testString = "These terms should go in the term vector";
+		Documento doc1 = new Documento("src/test/resources/testDiccionario/testAddDictionaryEntry.txt");
+		String testString = "These These terms should should go in the term vector";
 		
-		
-		
+		/* Since we are just testing this method by itself, we are introducing a dummy string.
+		 * The proper functionality of this method should be tested in an integration test,
+		 * where the class Indizador is the one to call it, after cleaning up the terms.
+		 */
 		dicc.addDictionaryEntry(doc1, testString);
 
+		Set<String> expected = new HashSet<String>();
+		expected.add("These");
+		expected.add("terms");
+		expected.add("should");
+		expected.add("go");
+		expected.add("in");
+		expected.add("the");
+		expected.add("term");
+		expected.add("vector");
+		
+		Set<String> result = dicc.getAllTerms().keySet();
+		
+		assertEquals(expected, result);
+	}
+	
+	/**
+	 * TODO: The method in the dictionary to GetTermOccurence depends heavily on the filters applied by the indexer.
+	 * This should probably be tested in an integration test with both classes interacting or think of a better
+	 * way to uncouple the design of these two classes. 
+	 */
+	@Test
+	public void testGetTermOccurrenceInDocument() {
+		Documento doc1 = new Documento("src/test/resources/testDiccionario/testGetTermOccurrenceInDocument1.txt");
+		Documento doc2 = new Documento("src/test/resources/testDiccionario/testGetTermOccurrenceInDocument2.txt");
+		
+		String testString1 = "These These terms should should go in the term vector";
+		String testString2 = "This is is another another another term string to test";
+		
+		
+		
+		dicc.addDictionaryEntry(doc1, testString1);
+		dicc.addDictionaryEntry(doc2, testString2);
+		
+//		Entry entry = dicc.getAllTerms().get("These").get(0);
+//		System.out.println(entry);
+		
+		assertEquals(0, dicc.getTermOccurrenceInDocument("These", doc1));
+		assertEquals(2, dicc.getTermOccurrenceInDocument("terms", doc1));
+		assertEquals(2, dicc.getTermOccurrenceInDocument("should", doc1));
+		
+		assertEquals(0, dicc.getTermOccurrenceInDocument("This", doc2));
+		assertEquals(2, dicc.getTermOccurrenceInDocument("is", doc2));
+		assertEquals(3, dicc.getTermOccurrenceInDocument("another", doc2));
+//		assertEquals(-1, dicc.getTermOccurrenceInDocument("Non-existant", doc2));
 	}
 
 }
--- ModeloVectorial_4/src/test/java/com/RAI/ModeloVectorial/transformacion/InidizadorTest.java
+++ ModeloVectorial_4/src/test/java/com/RAI/ModeloVectorial/transformacion/InidizadorTest.java
@@ -2,6 +2,11 @@ package com.RAI.ModeloVectorial.transformacion;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -9,6 +14,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.RAI.ModeloVectorial.core.Documento;
+import com.RAI.ModeloVectorial.diccionario.Diccionario;
+import com.RAI.ModeloVectorial.diccionario.Entry;
 
 public class InidizadorTest {
 	
@@ -46,6 +53,82 @@ public class InidizadorTest {
 		assertEquals(occurrencesTimes, resultOccurrencesWords);
 		assertEquals(occurrencesWords, resultOccurrencesTimes);
 		assertEquals(occurrencesRepeated, resultOccurrencesRepeated);
+		
+	}
+	
+	/**
+	 * Checks to see that an array of documents is indexed properly by the Indizador class into a Diccionario object.
+	 */
+	@Test
+	public void integrationTestIndizar01() {
+		Diccionario dicc = new Diccionario();
+
+		Documento doc1 = new Documento("src/test/resources/testDiccionario/testGetTermOccurrenceInDocument1.txt");
+		Documento doc2 = new Documento("src/test/resources/testDiccionario/testGetTermOccurrenceInDocument2.txt");
+		Documento[] arrayDocs = {doc1, doc2};
+		
+		// Contents of the documents as Strings.
+		String testString1 = "These These terms should should go in the term vector";
+		String testString2 = "This is is another another another term string to test";
+		
+		// Index all the documents into the array
+		Indizador.indizar(arrayDocs, dicc);
+		
+		// Create duplicate strings that go through the same filtering process as the document in the "indizar" method.
+		String cleanString1 = Indizador.tokenizarTerminos(testString1);
+		cleanString1 = Indizador.stemTerminos(cleanString1);
+		
+		String cleanString2 = Indizador.tokenizarTerminos(testString2);
+		cleanString2 = Indizador.stemTerminos(cleanString2);
+		
+		// Create a set of strings containing the expected terms of each document.
+		Set<String> expectedTerms1 = new HashSet<String>(Arrays.asList(cleanString1.split(" ")));
+		Set<String> expectedTerms2 = new HashSet<String>(Arrays.asList(cleanString2.split(" ")));
+		
+		System.out.println("expectedTerms1: " + expectedTerms1);
+		System.out.println("expectedTerms2: " + expectedTerms2);
+		
+		// Create a set which will contain all results of both previous sets.
+		Set<String> expectedTermsTotal = new HashSet<String>();
+		expectedTermsTotal.addAll(expectedTerms1); 
+		expectedTermsTotal.addAll(expectedTerms2); 
+		
+		System.out.println("expectedTermsTotal: " + expectedTermsTotal);
+		System.out.println("dicc.getTermList(): " +dicc.getTermList());
+//		Set<String> resultTerms1 = dicc.getTermList();
+		
+		assertEquals(expectedTermsTotal, dicc.getTermList());
+	}
+	
+	@Test
+	public void integrationTestIndizar02() {
+		Diccionario dicc = new Diccionario();
+
+		Documento doc1 = new Documento("src/test/resources/testDiccionario/testGetTermOccurrenceInDocument1.txt");
+		Documento doc2 = new Documento("src/test/resources/testDiccionario/testGetTermOccurrenceInDocument2.txt");
+		Documento[] arrayDocs = {doc1, doc2};
+		
+		// Contents of the documents as Strings.
+		String testString1 = "These These terms should should go in the term vector";
+		String testString2 = "This is is another another another term string to test";
+		
+		// Index all the documents into the array
+		Indizador.indizar(arrayDocs, dicc);
+		
+		// Now we must check if the occurrences of each term in different documents are stored properly.
+		
+		// In the first document
+		int occurrenceTerm = 2;
+		int occurrenceVector = 1;
+		int occurrenceNull = -1;
+		
+		// In the second document
+		int occurrenceAnother = 3;
+		int occurrenceTest = 1;
+		int occurrenceNone = -1;
+		
+		assertEquals(occurrenceTerm, dicc.getTermOccurrenceInDocument("term", doc1));
+		
 		
 	}
 
