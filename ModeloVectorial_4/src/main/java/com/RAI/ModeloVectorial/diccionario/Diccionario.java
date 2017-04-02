d2a438a5b67d7c94f5871a4dad9a40d69c4269f7
vdegou <vdegou@gmail.com>
2017-04-02 19:32:12 +0200

    Changes to Diccionario class as well as tests.

--- ModeloVectorial_4/src/main/java/com/RAI/ModeloVectorial/diccionario/Diccionario.java
+++ ModeloVectorial_4/src/main/java/com/RAI/ModeloVectorial/diccionario/Diccionario.java
@@ -4,6 +4,7 @@ import com.RAI.ModeloVectorial.core.Documento;
 import com.RAI.ModeloVectorial.transformacion.Indizador;
 
 import java.util.HashMap;
+import java.util.Set;
 import java.util.Vector;
 
 public class Diccionario {
@@ -49,9 +50,15 @@ public class Diccionario {
 
     private void addTerm(Documento doc, String toAdd){
 
+    	// If the string is empty (to filter stopwords), jump out of this method.
+    	if (toAdd.equals("")) {
+    		return;
+    	}
+    	
         //Ya existe el termino
         if (allTerms.containsKey(toAdd)){
 
+//        	 Consultar con Kevin
             Vector<Entry> entryList = allTerms.get(toAdd);
             //El termino existe, pero es la primera ocurrencia en este documento
             for (Entry e : entryList){
@@ -61,16 +68,92 @@ public class Diccionario {
             }
             entryList.add(new Entry(doc, Indizador.getTermOccurrence(toAdd, doc)));
             return;
+            
             //El termino existe, y no es la primera ocurrencia
-
+			
+        	
+    		/* Finds the entry which references the document provided as a parameter.
+    		 * First, obtain all the entries in the dictionary that reference the term.
+    		 * Then, iterate through those entries and check which one of them references
+    		 * the document provided as parameter.
+    		 * Add 1 to the occurrences of the term in that document once found.
+    		 */
+        	
+        	/*
+        	Vector<Entry> entryList = allTerms.get(toAdd);
+        	for (Entry e : entryList) {
+        		if (e.getDocument() == doc) {
+        			e.increaseCount();
+        		}
+        	}
+        */	
         }
+        
         //No existe el termino
         else {
+//            Entry entry = new Entry(doc, Indizador.getTermOccurrence(toAdd, doc));
             Entry entry = new Entry(doc, Indizador.getTermOccurrence(toAdd, doc));
+            
+            /* It is possible that getTermOccurrence changes the Term (mainly because it calls the
+             * method which removes stopwords) to "". We do not want the empty string in our dictionary
+             * because other terms unassociated to the original one may be reduced to the empty string as well
+             * and we would get false data. 
+             * For example: "These" is reduced to "" because of the stopword filter. Another word such as "an"
+             * may also be reduced to "", increasing the count of that Entry (which should not exist in the first
+             * place). For this reason, we only add strings that have not been classified as stopwords.
+             */
+              
             Vector <Entry> newEntry = new Vector<Entry>();
             newEntry.add(entry);
             allTerms.put(toAdd, newEntry);
         }
     }
-
+    
+    /**
+     * Returns the whole dictionary structure.
+     * @return
+     */
+    public HashMap<String, Vector<Entry>> getAllTerms() {
+		return allTerms;
+	}
+    
+    /**
+     * Returns all the terms in the dictionary as a set of strings.
+     * @return
+     */
+    public Set<String> getTermList() {
+    	return allTerms.keySet();
+    }
+ 
+    /**
+     * Returns the amount of occurrences of a given term in a given document.
+     * @param term
+     * @param document
+     * @return
+     */
+    public int getTermOccurrenceInDocument(String term, Documento document) {
+    	
+    	// If the term doesn't exist in the dictionary, return -1.
+    	if (!allTerms.containsKey(term)) {
+    		return -1;
+    	}
+    	
+    	// Obtain all the entries for the term
+    	Vector<Entry> entries= allTerms.get(term);
+    	
+    	/* Iterate through all the entries, looking for the one 
+    	 * which matches the document sent as a parameter.
+    	 */
+    	for (Entry e : entries) {
+    		// Once found, return the amount of occurrences in that document.
+    		if (e.getDocument() == document) {
+    			return e.count;
+    		}
+    	}
+    	
+    	// If no term-document combination is found in the dictionary, return -1.
+    	return -1;
+    }
 }
+
+
--- ModeloVectorial_4/src/test/java/com/RAI/ModeloVectorial/diccionario/DiccionarioTest.java
+++ ModeloVectorial_4/src/test/java/com/RAI/ModeloVectorial/diccionario/DiccionarioTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.lucene.index.Term;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -13,6 +14,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.RAI.ModeloVectorial.core.Documento;
+import com.RAI.ModeloVectorial.transformacion.Indizador;
 
 public class DiccionarioTest {
 	private Diccionario dicc = new Diccionario();
@@ -60,10 +62,16 @@ public class DiccionarioTest {
 	}
 	
 	/**
+	 * TODO: Test the method AddTerm() before continuing with termOccurrence. There is a filtering issue.
+	 */
+	
+	/**
 	 * TODO: The method in the dictionary to GetTermOccurence depends heavily on the filters applied by the indexer.
 	 * This should probably be tested in an integration test with both classes interacting or think of a better
 	 * way to uncouple the design of these two classes. 
 	 */
+	
+	/*
 	@Test
 	public void testGetTermOccurrenceInDocument() {
 		Documento doc1 = new Documento("src/test/resources/testDiccionario/testGetTermOccurrenceInDocument1.txt");
@@ -72,22 +80,58 @@ public class DiccionarioTest {
 		String testString1 = "These These terms should should go in the term vector";
 		String testString2 = "This is is another another another term string to test";
 		
+		// We have to put the test strings through the same filtering process and check if we get the same results
+//		String testString1 = Indizador.tokenizarTerminos(testString1);
+//		testString1 = Indizador.stemTerminos(testString1);
+		
+		// Store the occurrences of each term in the first document.
+		int occurrencesThese = StringUtils.countMatches(testString1, "These");
+		int occurrencesTerms = StringUtils.countMatches(testString1, "terms");
+		int occurrencesShould = StringUtils.countMatches(testString1, "should");
+		int occurrencesGo = StringUtils.countMatches(testString1, "go");
+		int occurrencesIn = StringUtils.countMatches(testString1, "in");
+		int occurrencesThe = StringUtils.countMatches(testString1, "the");
+		int occurrencesTerm = StringUtils.countMatches(testString1, "term");
+		int occurrencesVector = StringUtils.countMatches(testString1, "vector");
 		
 		
+		System.out.println("testString1: " + testString1);
+		
+		// Add the documents to the dictionary
 		dicc.addDictionaryEntry(doc1, testString1);
 		dicc.addDictionaryEntry(doc2, testString2);
 		
-//		Entry entry = dicc.getAllTerms().get("These").get(0);
-//		System.out.println(entry);
 		
-		assertEquals(0, dicc.getTermOccurrenceInDocument("These", doc1));
-		assertEquals(2, dicc.getTermOccurrenceInDocument("terms", doc1));
-		assertEquals(2, dicc.getTermOccurrenceInDocument("should", doc1));
+		int occTerms = dicc.getTermOccurrenceInDocument("terms", doc1);
+		int occShould = dicc.getTermOccurrenceInDocument("should", doc1);
+		int occGo = dicc.getTermOccurrenceInDocument("go", doc1);
 		
-		assertEquals(0, dicc.getTermOccurrenceInDocument("This", doc2));
-		assertEquals(2, dicc.getTermOccurrenceInDocument("is", doc2));
-		assertEquals(3, dicc.getTermOccurrenceInDocument("another", doc2));
+		System.out.println("occurrencesTerms: " + occurrencesTerms + "occTerms: " + occTerms);
+		System.out.println("occurrencesShould: " + occurrencesShould + "occShould: " + occShould);
+		System.out.println("occurrencesGo: " + occurrencesGo+ "occGo: " + occGo);
+		
+		System.out.println(dicc.getAllTerms().keySet());
+		
+		if (occurrencesTerms == dicc.getTermOccurrenceInDocument("terms", doc1)) {
+			System.out.println("Success");
+		}
+		
+		assertEquals(occurrencesThese, dicc.getTermOccurrenceInDocument("These", doc1));
+		assertEquals(occurrencesTerms, dicc.getTermOccurrenceInDocument("terms", doc1));
+		assertEquals(occurrencesShould, dicc.getTermOccurrenceInDocument("should", doc1));
+		assertEquals(occurrencesGo, dicc.getTermOccurrenceInDocument("go", doc1));
+		assertEquals(occurrencesIn, dicc.getTermOccurrenceInDocument("in", doc1));
+		assertEquals(occurrencesThe, dicc.getTermOccurrenceInDocument("the", doc1));
+		assertEquals(occurrencesTerm, dicc.getTermOccurrenceInDocument("term", doc1));
+		assertEquals(occurrencesVector, dicc.getTermOccurrenceInDocument("vector", doc1));
+		
+		
+		
+//		assertEquals(0, dicc.getTermOccurrenceInDocument("This", doc2));
+//		assertEquals(2, dicc.getTermOccurrenceInDocument("is", doc2));
+//		assertEquals(3, dicc.getTermOccurrenceInDocument("another", doc2));
 //		assertEquals(-1, dicc.getTermOccurrenceInDocument("Non-existant", doc2));
 	}
+	*/
 
 }
