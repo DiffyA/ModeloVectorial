e52c361495e805d2a3bdfe42f94614ebc43a4cc7
vdegou <vdegou@gmail.com>
2017-04-13 18:36:25 +0200

    Changes to Occurrence class.
    Renamed Occurrence class to Occurrences. Added unit tests in
    OccurrencesTest.java.

---
+++ ModeloVectorial_4/src/main/java/com/RAI/ModeloVectorial/core/Occurrences.java
@@ -0,0 +1,56 @@
+package com.RAI.ModeloVectorial.core;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class Occurrences {
+	private HashMap<Documento, Integer> occurrences = new HashMap<Documento, Integer>();
+
+	
+	/**
+	 * Returns the amount of occurrences in a given document.
+	 * @param doc
+	 * @return
+	 */
+    public int getOccurrencesInDocument(Documento doc) {
+    	if (!occurrences.containsKey(doc)) {
+    		return 0;
+    	}
+    	
+    	return occurrences.get(doc);
+	}
+    
+    /**
+     * Method used to increment the amount of occurrences
+     * for a given document. If there is no key in the hashmap
+     * for the specified document, the document will be added
+     * to the hashmap, and one occurrence will be specified.
+     * @param doc
+     */
+    public void addOccurrenceInDocument(Documento doc){
+    	if (!occurrences.containsKey(doc)) {
+    		occurrences.put(doc, 1);
+    	}
+    	else {
+    		int currentOccurrences = this.getOccurrences().get(doc);
+    		occurrences.put(doc, currentOccurrences + 1);
+    	}
+	}
+    
+    /**
+     * Returns a set of all the documents in which the term appears
+     * at least once.
+     * @return
+     */
+    public Set<Documento> getDocuments() {
+    	return occurrences.keySet();
+    }
+    
+    /**
+     * Returns the hashmap data structure containing the occurrences.
+     * @return
+     */
+    public HashMap<Documento, Integer> getOccurrences() {
+    	return occurrences;
+    }
+}
---
+++ ModeloVectorial_4/src/test/java/com/RAI/ModeloVectorial/Core/OccurrencesTest.java
@@ -0,0 +1,110 @@
+package com.RAI.ModeloVectorial.Core;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.RAI.ModeloVectorial.core.Documento;
+import com.RAI.ModeloVectorial.core.Occurrences;
+
+public class OccurrencesTest {
+	Occurrences occurrences = new Occurrences();
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		occurrences.getOccurrences().clear();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddOccurrenceInDocument() {
+		Documento doc1 = new Documento("");
+		
+		// First check that the document does not exist inside the hashmap as a key.
+		assertFalse(occurrences.getOccurrences().containsKey(doc1));
+		
+		// Now we add it
+		occurrences.addOccurrenceInDocument(doc1);
+		
+		// Check that the HashMap now contains doc1 as a key.
+		assertTrue(occurrences.getOccurrences().containsKey(doc1));
+		
+		// We must also check that the occurrence count increased to 1.
+		assertEquals(1, (int) occurrences.getOccurrences().get(doc1));
+	}
+	
+	@Test
+	public void testGetDocuments() {
+		Documento doc1 = new Documento("");
+		Documento doc2 = new Documento("");
+		Documento doc3 = new Documento("");
+		
+		Set<Documento> expected = new HashSet<Documento>();
+		
+		//Add first document to both sets
+		occurrences.addOccurrenceInDocument(doc1);
+		expected.add(doc1);
+		
+		assertEquals(expected, occurrences.getDocuments());
+		
+		// Add second document to both sets
+		occurrences.addOccurrenceInDocument(doc2);
+		expected.add(doc2);
+		
+		assertEquals(expected, occurrences.getDocuments());
+		
+		//Try adding again the first document to the occurrences
+		occurrences.addOccurrenceInDocument(doc1);
+		
+		assertEquals(expected, occurrences.getDocuments());
+		
+		//Add the last document to both sets
+		occurrences.addOccurrenceInDocument(doc3);
+		expected.add(doc3);
+		
+		assertEquals(expected, occurrences.getDocuments());
+	}
+	
+	@Test
+	public void testGetOccurrencesInDocument() {
+		Documento doc1 = new Documento("");
+		Documento doc2 = new Documento("");
+		
+		// Check that for a non-existing document, the occurrences are 0.
+		assertEquals(0, occurrences.getOccurrencesInDocument(doc1));
+		assertEquals(0, occurrences.getOccurrencesInDocument(doc2));
+		
+		// Now, start adding occurrences and check.
+		occurrences.addOccurrenceInDocument(doc1);
+		assertEquals(1, occurrences.getOccurrencesInDocument(doc1));
+		
+		occurrences.addOccurrenceInDocument(doc2);
+		assertEquals(1, occurrences.getOccurrencesInDocument(doc2));
+		
+		occurrences.addOccurrenceInDocument(doc2);
+		assertEquals(2, occurrences.getOccurrencesInDocument(doc2));
+		
+		occurrences.addOccurrenceInDocument(doc1);
+		assertEquals(2, occurrences.getOccurrencesInDocument(doc1));
+	}
+
+}
