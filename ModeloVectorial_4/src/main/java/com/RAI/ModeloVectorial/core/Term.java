1b3af22efed359b5b33a06504cae08d752fc3f48
vdegou <vdegou@gmail.com>
2017-04-13 16:41:42 +0200

    Added the Term class.

--- ModeloVectorial_4/src/main/java/com/RAI/ModeloVectorial/core/Term.java
+++ ModeloVectorial_4/src/main/java/com/RAI/ModeloVectorial/core/Term.java
@@ -1,5 +1,15 @@
 package com.RAI.ModeloVectorial.core;
 
 public class Term {
-
+	private String term;
+	private int IDF;
+	
+	public Term(String term) {
+		this.term = term;
+		this.IDF = 0;
+	}
+	
+	public void increaseIDF() {
+		this.IDF++;
+	}
 }
