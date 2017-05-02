f1d0928fb1bf29d61dae09fc739736c1fd187062
KNGeeting <geeting@usc.edu>
2017-05-02 13:17:14 +0200

    CosineTFCalculator

--- ModeloVectorial_4/src/main/java/com/RAI/ModeloVectorial/similiarities/CosineTFCalculator.java
+++ ModeloVectorial_4/src/main/java/com/RAI/ModeloVectorial/similiarities/CosineTFCalculator.java
@@ -1,12 +1,34 @@
 package com.RAI.ModeloVectorial.similiarities;
 
+import com.RAI.ModeloVectorial.core.Term;
 import com.RAI.ModeloVectorial.logic.DocumentVector;
+
+import static java.lang.Math.pow;
 
 /**
  * Created by kgeetz on 4/26/17.
  */
 public class CosineTFCalculator implements Calculator {
     public double calculate(DocumentVector docVec, DocumentVector queryVec) {
-        return 0;
+
+        Calculator calc = new ScalarProductTFCalculator();
+        double scalarProduct = calc.calculate(docVec,queryVec);
+
+        double divisor = 0;
+        double queryDivisor = 0;
+
+        for (Term t : docVec.getVector().keySet()){
+            divisor += pow(docVec.getVector().get(t), 2);
+        }
+
+
+        for (Term t : queryVec.getVector().keySet()){
+            queryDivisor += pow(queryVec.getVector().get(t), 2);
+        }
+
+        divisor = Math.sqrt(divisor);
+        divisor *= Math.sqrt(queryDivisor);
+
+        return scalarProduct/divisor;
     }
 }
